86bbb42a5ace8c0354eee5754554d4dd5af412d7
Majinbua <61796606+Majinbua@users.noreply.github.com>
2023-04-27 23:13:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
     ami = "ami-00169914e6299b8e0"
-    instance_type = var.instancetype
+    instance_type = "t2.micro"
 
     tags = {
     Name = "my-first-ec2"
